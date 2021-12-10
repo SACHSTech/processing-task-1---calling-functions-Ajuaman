9d39378a88bdfa675e08f70c2485c11710be99e8
Ajuaman <tate.chin24@gmail.com>
2021-12-10 15:08:34 -0500

    Final Product

--- Sketch.java
+++ Sketch.java
@@ -11,29 +11,36 @@ public class Sketch extends PApplet {
 	}
 	  
 	public void setup() {
-	   background(210, 255, 173);
+	   background(125, 235, 250);
 	   strokeWeight(5);
 	}
 
 	public void draw() {
 		
 	   // BASE OF THE HOUSE
-	   rect(baseX, baseY, 200, 150);
 	   fill(138, 132, 127);
+	   rect(baseX, baseY, 200, 150);
 	   // DOOR
-	   rect(baseX + 80, baseY + 100, 30, 50);
 	   fill(118, 156, 145);
+	   rect(baseX + 80, baseY + 100, 30, 50);
 		
 	   // ROOF
-	   triangle(100, 400, 300, 80, 500, 400);
 	   fill(44, 245, 188);
+	   triangle(100, 400, 300, 80, 500, 400);
 		
 	   // WINDOWS
+	   fill(255);
 	   ellipse(350, 450, 30, 30);
 	   ellipse(250, 450, 30, 30);
-	   fill(255);
 		
 	   line(300, 300, 160, 300);
 	   line(300, 200, 223, 200);
+	   
+	   // GROUND
+	   fill(94, 219, 98);
+	   rect(0, 550, 600, 100);
+	   
+	   fill(242, 255, 59);
+	   ellipse(0, 0, 100, 100);
 	}
 }
